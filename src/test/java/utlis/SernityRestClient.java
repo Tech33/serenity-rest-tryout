75f71707e92b1cf0578963b965bdad23ecd6af2e
Saurabh <kakkar33@gmail.com>
2024-03-10 20:03:11 +0000

    Adding Common RestAssured method

---
+++ src/test/java/utlis/SernityRestClient.java
@@ -0,0 +1,28 @@
+package utlis;
+
+/*
+public class SernityRestClient {
+
+    static EnvironmentVariables environmentVariables = SerenityInfrastructure.getEnvironmentVariables();
+    public static Response sendRequst(HttpMethod method, String endpoint, Map<String, Object> headers,  Map<String, Object> paramerets, RequestBody body, boolean isAuthReq)
+
+    {
+        RestAssured.baseURI= environmentVariables.getProperty("restapi.baseurl");
+        Response res = SerenityRest.rest().given().when().headers()
+                switch(method)
+                    get:
+                .get("/pet/findByStatus?status="+status);
+
+        return res;
+
+    }
+
+    private Headers buildHeaders (Map <String, Object> headers)
+    {
+       Builder builder = new Builder();
+        Headers buildHeaders = new Headers(builder);
+
+
+    }
+}
+*/
